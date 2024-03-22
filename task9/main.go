1492a9122283fe2904ed84dfcda2e829304bb998
sunder <evenquantity@gmail.com>
2024-03-22 13:33:34 +0300

    task9: done

---
+++ task9/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{12, 545, 76, 163, 6752, 326, 6283}
+
+	// Channel for nums from array
+	numsChan := make(chan int, len(nums))
+
+	// Write nums to channel
+	for _, num := range nums {
+		numsChan <- num
+	}
+	// Close channel because we don't need to write here anymore
+	close(numsChan)
+
+	// Channel for doubled nums
+	doubledNumsChan := make(chan int, len(nums))
+
+	// Write doubled nums to channel
+	for num := range numsChan {
+		doubledNumsChan <- num * 2
+	}
+
+	// Close channel because we don't need to write here anymore
+	close(doubledNumsChan)
+
+	// Read and print nums from doubled channel
+	for num := range doubledNumsChan {
+		fmt.Println(num)
+	}
+}
