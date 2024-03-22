d2cc89a16d666d279e43236e7519b9493229a914
sunder <evenquantity@gmail.com>
2024-03-22 14:42:03 +0300

    task13: done

---
+++ task13/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func swap(first, second int) (int, int) {
+	return second, first
+}
+
+func main() {
+	a, b := 23, 54
+
+	fmt.Printf("a = %d, b = %d\n", a, b)
+
+	// a, b = b, a
+	a, b = swap(a, b)
+
+	fmt.Printf("Swapped: a = %d, b = %d\n", a, b)
+}
