5a5563e3d8d840ad7e4e7c7cfb68c7e9a8e2abb4
sunder <evenquantity@gmail.com>
2024-03-23 22:34:31 +0300

    task20: done

---
+++ task20/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"strings"
+)
+
+func invertWords(str *string) string {
+	// Create string builder
+	builder := strings.Builder{}
+
+	// Split string to words
+	splitted := strings.Split(*str, " ")
+
+	for i := len(splitted) - 1; i >= 0; i-- {
+		// Write string to builder and add space separator
+		_, err := builder.WriteString(splitted[i] + " ")
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	// Trim the last space and build string
+	return strings.TrimSuffix(builder.String(), " ")
+}
+
+func main() {
+	str := "snow dog sun"
+	fmt.Println(str)
+
+	str = invertWords(&str)
+	fmt.Println(str)
+}
