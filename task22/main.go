5647d51223329c189287357ec5779cd41969ecd6
sunder <evenquantity@gmail.com>
2024-03-23 12:09:23 +0300

    task22: done

---
+++ task22/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"math/big"
+)
+
+func main() {
+	// Create two big nums
+	a := big.NewFloat(34523453462)
+	b := big.NewFloat(61423563456)
+
+	// Create add operation
+	add := new(big.Float)
+	added := add.Add(a, b)
+	fmt.Printf("%f + %f = %f\n", a, b, added)
+
+	// Create subtraction operation
+	sub := new(big.Float)
+	subtracted := sub.Sub(a, b)
+	fmt.Printf("%f - %f = %f\n", a, b, subtracted)
+
+	// Create division operation
+	div := new(big.Float)
+	divided := div.Quo(a, b)
+	fmt.Printf("%f / %f = %f\n", a, b, divided)
+
+	// Create multiplication operation
+	mul := new(big.Float)
+	multiplicated := mul.Mul(a, b)
+	fmt.Printf("%f * %f = %f\n", a, b, multiplicated)
+}
